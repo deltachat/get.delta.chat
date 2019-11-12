83e872f1be9674e319268b64af3d91a14502d74e
Simon Laux <mobile.info@simonlaux.de>
2019-11-13 00:27:02 +0100

    some cleanup

--- src/logic.js
+++ src/logic.js
@@ -1,36 +1,17 @@
 function main() {
-    //TODO: add alt text comming soon to each element with soon class
-
-    const soon = document.getElementsByClassName("soon")
-    for (let i = 0; i < soon.length; i++) {
-        const element = soon[i];
-        element.setAttribute('title', "Comming Soon!")
-
+    var soon = document.getElementsByClassName("soon");
+    for (var i = 0; i < soon.length; i++) {
+        var element = soon[i];
+        element.setAttribute('title', "Comming Soon!");
     }
 
-    console.log(navigator.userAgent, userAgentToOS())
-
-    var list = document.getElementById("boxes");
     var recommend = document.getElementById("recommend");
+    recommend.appendChild(document.getElementById(userAgentToOS()));
 
-    const platforms = {
-        android: document.getElementById('android'),
-        ios: document.getElementById('ios'),
-        linux: document.getElementById('linux'),
-        windows: document.getElementById('windows'),
-        osx: document.getElementById('osx')
-    }
-
-    recommend.appendChild(platforms[userAgentToOS()])
-
-
-
-    //TODO: reorder elements / recomend based on user agent
 }
 
-
 function userAgentToOS() {
-    const uA = navigator.userAgent
+    var uA = navigator.userAgent;
     if (uA.includes('iPad') && uA.includes('Mobile'))
         return 'ios'
     else if (uA.includes('iPhone') && uA.includes('Mobile'))
@@ -43,7 +24,4 @@ function userAgentToOS() {
         return 'windows'
     else if (uA.includes('Macintosh') || uA.includes('Mac OS X'))
         return 'osx'
-
 }
-
-window.onload = main
